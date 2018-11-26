991831c2d405bf5353903848ecfe99181a61c58d
Olaf Gałązka <ulff.pl@gmail.com>
2018-11-26 23:00:02 +0100

    added dev script

--- src/controllers/input.js
+++ src/controllers/input.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/times',  (req, res) => {
   console.log(`Received time: ${req.body.time}`);
   res.status(201).json({
+    id: 'e86556be-ef86-4a6e-92f3-2013da9aa5b2',
     time: req.body.time,
   });
 });
