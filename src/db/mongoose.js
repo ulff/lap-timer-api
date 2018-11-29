7a10fe994b8cc5d68a802ced328e16b47614756b
Olaf Gałązka <ulff@esgaroth.local>
2018-11-29 23:51:36 +0100

    repaired mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://mongo:27017/LapTimerApi');
 
-module.exports = { mongoose };
+module.exports = mongoose;
--- src/model/lap.js
+++ src/model/lap.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('../db/mongoose');
 
 const Lap = mongoose.model('Lap', {
   time: {
