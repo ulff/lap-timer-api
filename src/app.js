0fe11ec8cf5f2594c96944fe597c579cfdbd5d0a
Olaf Gałązka <ulff.pl@gmail.com>
2018-12-02 00:09:24 +0100

    added handlebars, added content negotiation

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path");
+
 const helloController = require('./controllers/hello');
 const inputController = require('./controllers/input');
-const statsController = require('./controllers/stats');
+const resultsController = require('./controllers/results');
 
 const app = express();
 app.disable('x-powered-by');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(bodyParser.json());
 
 app.use(helloController);
 app.use(inputController);
-app.use(statsController);
+app.use(resultsController);
 
 module.exports = app;
---
+++ src/controllers/results.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { Lap } = require('../model/lap');
+
+const router = express.Router();
+
+router.get('/results', (req, res) => {
+  Lap.find().then((results) => {
+    res.format({
+      "text/html"() {
+        res.status(200).render("results", { results });
+      },
+      "application/json"() {
+        res.status(200).json(results);
+      },
+      default() {
+        res.status(200).json(results);
+      }
+    });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+module.exports = router;
