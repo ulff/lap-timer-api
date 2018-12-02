153d7faacf746d45b775d130a9c9f6fad5d2afb2
Olaf Gałązka <ulff.pl@gmail.com>
2018-12-02 23:17:46 +0100

    results per session

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -21,4 +21,22 @@ router.get('/results', (req, res) => {
   });
 });
 
+router.get('/results/sessions/:slug', (req, res) => {
+  Lap.find({session: req.params.slug}).then((results) => {
+    res.format({
+      "text/html"() {
+        res.status(200).render("results", { results });
+      },
+      "application/json"() {
+        res.status(200).json(results);
+      },
+      default() {
+        res.status(200).json(results);
+      }
+    });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 module.exports = router;
